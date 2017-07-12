968c34eda7b489e1eb7eda28a5fd52b62b36f962
croacker <7Rv5699400>
2017-07-12 20:26:35 +0800

    add stubs

---
+++ devices/atol-fr/src/main/java/com/invariant/devices/atolfr/fr/Fr.java
@@ -0,0 +1,7 @@
+package com.invariant.devices.atolfr.fr;
+
+/**
+ * Created by agumenyuk on 12.07.2017.
+ */
+public interface Fr {
+}
---
+++ devices/atol-fr/src/main/java/com/invariant/devices/atolfr/service/SerialPortService.java
@@ -0,0 +1,7 @@
+package com.invariant.devices.atolfr.service;
+
+/**
+ * Created by agumenyuk on 12.07.2017.
+ */
+public class SerialPortService {
+}
